95bc255c9d416edd1b752d1a009c1d8c9343ebd5
Julia Gogonova <gogonova2010@yandex.ru>
2024-04-30 01:13:56 +0300

    commit

--- src/Components/MovieCard/index.jsx
+++ src/Components/MovieCard/index.jsx
@@ -7,6 +7,19 @@ import NotFound from '../NotFound';
 import styles from './MovieCard.module.scss';
 
 const MovieCard = ({ movie }) => {
+  const color = () => {
+    if (movie.vote_average <= 3) {
+      return 'red';
+    }
+    if (movie.vote_average > 3 && movie.vote_average <= 5) {
+      return 'orange';
+    }
+    if (movie.vote_average > 5 && movie.vote_average <= 7) {
+      return 'yellow';
+    }
+    return 'green';
+  };
+  const ratingColor = styles[color()];
   return (
     <Col className={styles.col}>
       <div className={styles.card}>
@@ -17,7 +30,10 @@ const MovieCard = ({ movie }) => {
         )}
 
         <div className={styles.info}>
-          <h3>{movie.title}</h3>
+          <div className={styles.flex}>
+            <h3>{movie.title}</h3>
+            <div className={`${ratingColor} ${styles.rating}`}>{movie.vote_average.toFixed(1)}</div>
+          </div>
           {movie.release_date ? <p className={styles.date}>{format(parseISO(movie.release_date), 'MMMM d, y')}</p> : ''}
           <div className={styles.genres}>
             {movie.genres.map((genre) => (
